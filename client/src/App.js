3e870f11da15c501355d6453c091177a97f8f4a9
Shishir <shishir.mishra88@gmail.com>
2020-04-25 00:11:08 -0500

    basic working app

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+import NoMatch from "./Pages/NoMatch";
+import Search from "./Pages/Search";
+import Saved from "./Pages/Saved";
+
+import "./App.css";
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
+
+export default App;
